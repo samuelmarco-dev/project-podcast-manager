6aa9ff6fce87fe8875196089910a51f060fced12
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2024-08-20 06:36:59 -0300

    feat: podcast interface; inicial array data podcasts

---
+++ src/schemas/episodeSchema.ts
@@ -0,0 +1,12 @@
+interface episode {
+    id: string;
+    podcast: string;
+    name: string;
+    duration: string;
+    url: string;
+    image: string;
+    published_at: string;
+    categories: string[];
+}
+
+export default episode;
